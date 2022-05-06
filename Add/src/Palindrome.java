b78f7072b168836eee45b747c647c3dde176aa8c
Admin <Admin@DESKTOP-8HEG7QS>
2022-05-06 20:14:06 +0530

    Method Created

--- Add/src/Palindrome.java
+++ Add/src/Palindrome.java
@@ -1,4 +1,24 @@
 
 public class Palindrome {
+	
+	public static void main(String[] args)
+	{
+		String str = "Madam";
+		String rev="";
+		char[] ch = str.toCharArray();
+		
+		for(int i=str.length()-1;i>=0;i--)
+		{
+			rev+=ch[i];
+		}
+		if(rev.toLowerCase().equals(str.toLowerCase()))
+		{
+			System.out.println("palindrome");
+			
+		}
+		else
+			System.out.println("Not palindrome");
+		
+	}
 
 }
